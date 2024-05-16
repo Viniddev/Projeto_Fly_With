ccf42c70f93b8f5a07c108a39dae582f3b7a44d5
Vinicius Dias <vinicius.rodrigues@fiskaldigital.com.br>
2024-05-16 17:38:50 -0300

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
+  imports:[HeaderComponent],
+  standalone:true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
+  constructor(){}
   title = 'FinanceProject';
 }
