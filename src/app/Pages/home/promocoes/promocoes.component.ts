ea9c218e70912fde9cd154803c3eee7be34ce179
Raphael Nascimento <raphael.ofc11@gmail.com>
2024-06-10 09:42:15 -0300

    criaçao do sort

--- src/app/Pages/home/promocoes/promocoes.component.ts
+++ src/app/Pages/home/promocoes/promocoes.component.ts
@@ -8,11 +8,35 @@ import { PromocaoService } from 'src/app/Core/services/promocao.service';
 })
 export class PromocoesComponent implements OnInit {
   promocoes!: Promocao[];
+  originalPromocoes!: Promocao[];
   constructor(private service: PromocaoService) {}
 
   ngOnInit(): void {
     this.service.listar().subscribe((res) => {
       this.promocoes = res;
+      this.originalPromocoes = [...res];
     });
+  }
+
+  handleSortChange(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const value = selectElement.value;
+    this.onSortChange(value);
+  }
+
+  onSortChange(value: string): void {
+    if (value === 'alphabetical') {
+      this.sortAlphabetically();
+    } else if (value === 'price') {
+      this.sortByPrice();
+    }
+  }
+
+  sortAlphabetically(): void {
+    this.promocoes.sort((a, b) => a.destino.localeCompare(b.destino));
+  }
+
+  sortByPrice(): void {
+    this.promocoes.sort((a, b) => a.preco - b.preco);
   }
 }
